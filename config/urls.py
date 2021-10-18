f04ad96838f2d20fe728f1d9209970082f3c348c
MJbae <bmj5208@gmail.com>
2021-10-18 11:44:39 +0900

    docs: Apply schema package

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +23,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('openapi', get_schema_view(
+        title="Blog API",
+        description="A sample API for learning DRF",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
