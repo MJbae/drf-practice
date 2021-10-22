c30341bdad7f2f83a4412af18c1b8d4687aba540
MJbae <bmj5208@gmail.com>
2021-10-22 15:46:04 +0900

    test: add books test for counting sql calls

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.urls import reverse
+from django_seed import Seed
 
-# Create your tests here.
+from .models import Author, Book
+from rest_framework.test import APITestCase
+
+seeder = Seed.seeder()
+
+
+class BooksTestCase(APITestCase):
+    def test_list_books(self):
+        # Add dummy data to the Author and Book Table
+        seeder.add_entity(Author, 5)
+        seeder.add_entity(Book, 10)
+        seeder.execute()
+        # we expect the result in 1 query
+        with self.assertNumQueries(1):
+            response = self.client.get(reverse("book_list"), format="json")
