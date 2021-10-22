e1d4d8f7bc596de4b2215085a2762605dd070289
MJbae <bmj5208@gmail.com>
2021-10-22 15:48:50 +0900

    fix: change name of viewset in test

--- books/tests.py
+++ books/tests.py
@@ -8,11 +8,11 @@ seeder = Seed.seeder()
 
 
 class BooksTestCase(APITestCase):
-    def test_list_books(self):
+    def test_books(self):
         # Add dummy data to the Author and Book Table
         seeder.add_entity(Author, 5)
         seeder.add_entity(Book, 10)
         seeder.execute()
         # we expect the result in 1 query
         with self.assertNumQueries(1):
-            response = self.client.get(reverse("book_list"), format="json")
+            response = self.client.get(reverse("books"), format="json")
