01a628b34a0d0681b0eaef6f4806edff1f468861
MJbae <bmj5208@gmail.com>
2021-10-24 09:57:57 +0900

    feat: add PublicCattle model and migrate

--- cattles/models.py
+++ cattles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+# class to define choices
+class SexCategory(models.TextChoices):
+    COW = 'C'
+    OX = 'O'
+    BULL = 'B'
+
+
+class PublicCattle(models.Model):
+    id = models.CharField(max_length=12, primary_key=True)
+    birth_date = models.DateField
+    sex = models.CharField(
+        max_length=1,
+        choices=SexCategory.choices,
+    )
