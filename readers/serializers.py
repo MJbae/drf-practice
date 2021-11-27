641b95ac73435745f3773ce0d9f25abd81e5e91b
MJbae <bmj5208@gmail.com>
2021-11-27 19:53:38 +0900

    feat: revert single update and create

--- readers/serializers.py
+++ readers/serializers.py
@@ -5,36 +5,10 @@ from rest_framework.exceptions import NotFound
 from .models import Reader, Address
 
 
-class BulkListSerializer(serializers.ListSerializer):
-    def update(self, queryset, validated_data):
-        id_attr = getattr(self.child.Meta, 'update_lookup_field')
-        update_data = {i.get(id_attr): i for i in validated_data}
-
-        if not all((bool(i) and not inspect.isclass(i) for i in update_data.keys())):
-            raise NotFound(_('Could not find all objects to update'))
-
-        objects = queryset.filter(**{'{}__in'.format(id_attr): update_data.keys()})
-
-        if len(update_data) != objects.count():
-            raise NotFound(_('Could not find all objects to update'))
-
-        response = []
-        for id, data in update_data.items():
-            for obj in objects:
-                if str(getattr(obj, id_attr)) == str(id):
-                    response.append(self.child.update(obj, data))
-
-        return response
-
-
 class AddressSerializer(serializers.ModelSerializer):
-    address_pk = serializers.IntegerField()
-
     class Meta:
         model = Address
         fields = '__all__'
-        update_lookup_field = 'address_pk'
-        list_serializer_class = BulkListSerializer
 
 
 class ReaderSerializer(serializers.ModelSerializer):
--- readers/views.py
+++ readers/views.py
@@ -29,58 +29,37 @@ class AddressViewSet(viewsets.ModelViewSet):
             reader.save(update_fields=['latest_city'])
             request_body.update({"city": new_city})
 
-        if kwargs.pop("pk", None):
-            serializer = self.get_serializer(
-                instance=self.get_object(), data=request_body, **kwargs
-            )
-        else:
-            kwargs["many"] = isinstance(request_body, list)
-            serializer = self.get_serializer(
-                self.get_queryset(), data=request_body, **kwargs
-            )
+        if not kwargs.pop("pk", None):
+            serializer = self.get_serializer(data=request_body, **kwargs)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer.is_valid()
-        #     serializer.save()
-        #     return Response(serializer.data, status=status.HTTP_200_OK)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(instance=self.get_object(), data=request_body, **kwargs)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # Address 모델 인스턴스 생성에 따라 Reader의 연관 필드값이 동적으로 변함
     def create(self, request, *args, **kwargs):
         request_body = request.data
 
-        is_bulk = isinstance(request_body, list)
+        is_many = isinstance(request_body, list)
 
-        if is_bulk:
-            object_list_in_request = request_body
-            print(f'request_body: {request_body}')
-            for object_in_request in object_list_in_request:
-                reader_id = object_in_request.get('reader')
-                reader = Reader.objects.get(id=reader_id)
-                latest_city = getattr(reader, 'latest_city')
-                new_city = object_in_request.get('city')
-                print(f'latest_city: {latest_city}, new_city: {new_city}')
+        object_in_request = request_body
+        reader_id = object_in_request.get('reader')
+        reader = Reader.objects.get(id=reader_id)
+        latest_city = getattr(reader, 'latest_city')
+        new_city = object_in_request.get('city')
+        print(f'latest_city: {latest_city}, new_city: {new_city}')
 
-                # 새로 생성되는 address가 최신 city일 경우(가장 값이 높은 city일 경우)
-                # Reader 내 latest_city 값 수정
-                if new_city > latest_city:
-                    reader.latest_city = new_city
-                    reader.save()
+        # 새로 생성되는 address가 최신 city일 경우(가장 값이 높은 city일 경우)
+        # Reader 내 latest_city 값 수정
+        if new_city > latest_city:
+            reader.latest_city = new_city
+            reader.save()
 
-        else:
-            object_in_request = request_body
-            reader_id = object_in_request.get('reader')
-            reader = Reader.objects.get(id=reader_id)
-            latest_city = getattr(reader, 'latest_city')
-            new_city = object_in_request.get('city')
-            print(f'latest_city: {latest_city}, new_city: {new_city}')
-
-            # 새로 생성되는 address가 최신 city일 경우(가장 값이 높은 city일 경우)
-            # Reader 내 latest_city 값 수정
-            if new_city > latest_city:
-                reader.latest_city = new_city
-                reader.save()
-
-        kwargs["many"] = is_bulk
+        kwargs["many"] = is_many
         serializer = self.get_serializer(data=request_body, **kwargs)
 
         if serializer.is_valid():
