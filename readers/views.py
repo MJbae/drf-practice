4188ba28c114bffc8fac1c1ab4cff686684b12fd
MJbae <bmj5208@gmail.com>
2021-11-27 17:40:47 +0900

    feat: implement bulk create treating nested models

--- readers/views.py
+++ readers/views.py
@@ -47,23 +47,41 @@ class AddressViewSet(viewsets.ModelViewSet):
     # Address 모델 인스턴스 생성에 따라 Reader의 연관 필드값이 동적으로 변함
     def create(self, request, *args, **kwargs):
         request_body = request.data
-        model_relationship = isinstance(request_body, list)
-        reader_id = request_body.get('reader')
-        reader = Reader.objects.get(id=reader_id)
-        latest_city = getattr(reader, 'latest_city')
-        new_city = request_body.get('city')
-        print(f'latest_city: {latest_city}, new_city: {new_city}')
 
-        # 새로 생성되는 address가 최신 city일 경우(가장 값이 높은 city일 경우)
-        # Reader 내 latest_city 값 수정
-        if new_city > latest_city:
-            reader.latest_city = new_city
-            reader.save()
+        is_bulk = isinstance(request_body, list)
 
-        # 새로 생성되는 address의 city의 값이 가장 높지 않을 경우
-        # Reader 내 latest_city 값 유지
+        if is_bulk:
+            object_list_in_request = request_body
+            print(f'request_body: {request_body}')
+            for object_in_request in object_list_in_request:
+                reader_id = object_in_request.get('reader')
+                reader = Reader.objects.get(id=reader_id)
+                latest_city = getattr(reader, 'latest_city')
+                new_city = object_in_request.get('city')
+                print(f'latest_city: {latest_city}, new_city: {new_city}')
 
-        serializer = AddressSerializer(data=request_body, many=model_relationship)
+                # 새로 생성되는 address가 최신 city일 경우(가장 값이 높은 city일 경우)
+                # Reader 내 latest_city 값 수정
+                if new_city > latest_city:
+                    reader.latest_city = new_city
+                    reader.save()
+
+        else:
+            object_in_request = request_body
+            reader_id = object_in_request.get('reader')
+            reader = Reader.objects.get(id=reader_id)
+            latest_city = getattr(reader, 'latest_city')
+            new_city = object_in_request.get('city')
+            print(f'latest_city: {latest_city}, new_city: {new_city}')
+
+            # 새로 생성되는 address가 최신 city일 경우(가장 값이 높은 city일 경우)
+            # Reader 내 latest_city 값 수정
+            if new_city > latest_city:
+                reader.latest_city = new_city
+                reader.save()
+
+        kwargs["many"] = is_bulk
+        serializer = self.get_serializer(data=request_body, **kwargs)
 
         if serializer.is_valid():
             serializer.save()
