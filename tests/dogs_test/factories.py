1eef5986955405221e5bfd1107135e0e554e271c
MJbae <bmj5208@gmail.com>
2021-12-18 11:38:59 +0900

    test: change basic factory form

---
+++ tests/dogs_test/factories.py
@@ -0,0 +1,14 @@
+# factories.py
+import factory
+
+from factory.faker import faker
+from dogs.models import *
+
+
+class CurrencyFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Currency
+
+    name = factory.Sequence(lambda n: 'Bae%s' % n)
+    code = factory.Sequence(lambda n: '%s' % n)
+    symbol = factory.Sequence(lambda n: '%s' % n)
