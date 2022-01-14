c72854792a64632966980c5a0a0f79c4f968d3c9
MJbae <bmj5208@gmail.com>
2022-01-14 12:02:43 +0900

    feat: handle other-transaction, other-currency in admin

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Dog, Owner, Customer
+from .models import Dog, Owner, Customer, Transaction, OtherTransaction, Currency, OtherCurrency
 
 admin.site.register(Dog)
 admin.site.register(Owner)
 admin.site.register(Customer)
+admin.site.register(Currency)
+admin.site.register(OtherCurrency)
+admin.site.register(Transaction)
+admin.site.register(OtherTransaction)
